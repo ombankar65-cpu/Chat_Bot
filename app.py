48153b32d82adb1b8c17924ed86cab6d881b2978
Om Raju Bankar <ombankar65@gmail.com>
2026-03-04 10:26:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import streamlit as st
-import os
 from langchain.chat_models import init_chat_model
 
-st.title("Gemini 2.5 Flash Chatbot 🤖")
+st.title("Gemini Chatbot 🤖")
 
-# Get API key from Streamlit Secrets
-api_key = st.secrets["AIzaSyB6YhkmPcVg0ToZU85pOtYVgor3PQxUa_w"]
+# Correct way
+api_key = st.secrets["GOOGLE_API_KEY"]
 
-# Initialize model properly
 model = init_chat_model(
-    "google_genai:gemini-2.5-flash-lite",
+    "google_genai:gemini-1.5-flash",
     google_api_key=api_key
 )
 
@@ -17,5 +15,4 @@ user_input = st.text_input("Enter your prompt:")
 
 if user_input:
     response = model.invoke(user_input)
-    st.write("### Response:")
     st.write(response.content)
